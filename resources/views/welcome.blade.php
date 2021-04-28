afcc315e4fc3461a7f2c6c3f36cdf07bdfe752f1
unknown <johnnyrufinojr@gmail.com>
2021-04-28 15:19:43 +0800

    Fixing profile section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/dev.css') }}" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
   <title>Document</title>
 </head>
-<body>
+<body class="antialiased font-sans">
   <div class="relative min-h-screen md:flex">
 
     <!-- mobile menu bar -->
     <div class="bg-gray-800 text-gray-100 flex justify-between md:hidden">
       <!-- logo -->
-      <a href="#" class="block p-4 text-white font-bold">Better Dev</a>
+      <a href="#" class="block p-4 text-white font-bold" style="font-family: Lobster-Regular">ARPOCHI</a>
   
       <!-- mobile menu button -->
       <button class="mobile-menu-button p-4 focus:outline-none focus:bg-gray-700">
@@ -27,12 +28,14 @@
     <div class="sidebar bg-gray-800 text-blue-100 w-64 space-y-6 py-7 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
       <!-- logo -->
       <a href="#" class="flex items-center space-x-2 px-4">
-        <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
-        </svg>
+        <h2 style="font-family: Lobster-Regular" class="pl-3 text-2xl">ARPOCHI LOGO</h4>
       </a>
       <!-- nav -->
-      <nav class="text-sm subpixel-antialiased px-3">
+      <nav class="text-sm subpixel-antialiased px-5 pt-4">
+        <a href="#" class="block py-2 px-4 rounded transition duration-200 hover:bg-gray-700 hover:text-white bg-gray-700">
+          <svg class="w-4 h-4 inline mr-1 mb-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+          Home
+        </a>
         <a href="#" class="block py-2 px-4 rounded transition duration-200 hover:bg-gray-700 hover:text-white">
           <svg class="w-4 h-4 inline mr-1 mb-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"></path></svg>
           Reports
@@ -59,20 +62,67 @@
         </a>
         <a href="" class="block py-2 px-4 rounded transition duration-200 hover:bg-gray-700 hover:text-white">
           <svg class="w-4 h-4 inline mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-          Help
+          FAQ
         </a>
       </nav>
-      <div class="absolute inset-x-0 px-4 bottom-0 h-16 ...">
+      <div class="absolute inset-x-0 px-4 bottom-0 h-16 subpixel-antialiased text-sm">
         <img class="inline-block h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-        <span>Tim Cook</span>
+ 
+        <div class="absolute ml-12 top-0 h-16 ...">Alicia Vikander</div>
+        <div class="absolute ml-12 bottom-0 h-11 text-gray-400">alicia@sisyphus.com</div>
+        <div class="absolute right-0 top-0 h-16 mr-5">
+          <svg class="w-4 h-4 cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+        </div>
       </div>
     </div>
   
     <!-- content -->
     <div class="flex-1 p-10 text-2xl">
-      {{-- content goes here --}}
+        <!-- content -->
     </div>
-  
   </div>
+  <section x-data="{ open: true }" @keydown.window.escape="open = false" x-init="$watch(&quot;open&quot;, o => !o &amp;&amp; window.setTimeout(() => (null), 5000))" x-show="open" class="fixed inset-0 overflow-hidden" aria-labelledby="slide-over-title" x-ref="dialog" aria-modal="true">
+    <div class="absolute inset-0 overflow-hidden">
+      
+        <div x-show="open" x-transition:enter="ease-in-out duration-500" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in-out duration-500" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" x-description="Background overlay, show/hide based on slide-over state." class="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" @click="open = false" aria-hidden="true"></div>
+      
+      <div class="fixed inset-y-0 right-0 pl-10 max-w-full flex">
+        
+          <div x-show="open" x-transition:enter="transform transition ease-in-out duration-500 sm:duration-700" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transform transition ease-in-out duration-500 sm:duration-700" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full" class="relative w-screen max-w-md" x-description="Slide-over panel, show/hide based on slide-over state.">
+            
+              <div x-show="open" x-transition:enter="ease-in-out duration-500" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in-out duration-500" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" x-description="Close button, show/hide based on slide-over state." class="absolute top-0 left-0 -ml-8 pt-4 pr-2 flex sm:-ml-10 sm:pr-4">
+              
+              </div>
+            
+            <div class="h-full flex flex-col pb-6 bg-white shadow-xl overflow-y-scroll">
+              <div class="p-5 sm:px-6 min-h-24 bg-red-800 static" >
+                <div class="absolute top-5 right-8">
+                  <button class="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white" @click="open = false">
+                    <span class="sr-only">Close panel</span>
+                    <svg class="h-6 w-6" x-description="Heroicon name: outline/x" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                  </button>
+                </div>
+                <h2 class="text-lg font-medium text-white" id="slide-over-title">
+                  Personal Information
+                </h2>
+                <p class="text-sm mt-2 text-red-100">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus euismod quis viverra nibh cras pulvinar mattis.
+                </p>
+              </div>
+              <div class="mt-6 relative flex-1 px-4 sm:px-6">
+                <!-- Replace with your content -->
+                <div class="absolute inset-0 px-4 sm:px-6">
+                  <div class="h-full border-2 border-dashed border-gray-200" aria-hidden="true"></div>
+                </div>
+                <!-- /End replace -->
+              </div>
+            </div>
+          </div>
+        
+      </div>
+    </div>
+  </section>
 </body>
 </html>
