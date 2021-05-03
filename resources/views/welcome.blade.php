5f1f544294ff145e3784957a72fd05e1a30b99dd
unknown <johnnyrufinojr@gmail.com>
2021-05-03 15:37:53 +0800

    Added Hamburger Nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,8 +18,13 @@
       <a href="#" class="block py-2 text-white font-bold pl-6" style="font-family: Lobster-Regular">
         <img src="{{asset('img/outlined-logo.png')}}" height="80" width="80" alt="">
       </a>
-
+      <button class="mobile-menu-button p-4 focus:outline-none focus:bg-red-700">
+        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </button>
     </div>
+
     <!-- md sidebar button -->
     {{-- <div x-show="opensidebar" class="sidebar bg-gradient-to-t from-red-800 to-rose-800 text-red-100 w-20 space-y-6 py-7 absolute inset-y-0 left-0 transform -translate-x-full md:relative transition duration-200 ease-in-out">
       <!-- logo -->
@@ -95,7 +100,7 @@
 
      class="sidebar bg-gradient-to-t from-red-800 to-rose-800 text-red-100 w-64 space-y-6 py-7 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
       <!-- logo -->
-      <a href="#" class="flex items-center space-x-2 px-4 pl-9">
+      <a href="#" tabindex="-1" class="flex items-center space-x-2 px-4 pl-9">
         <img src="{{asset('img/outlined-logo.png')}}" height="175" width="175" alt="">
       </a>
       <!-- nav -->
@@ -104,42 +109,42 @@
         <div class="mb-3 pl-1">
           <span class="font-medium">NAVIGATION</span>
         </div>
-        <a href="#" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
+        <a href="#" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
           <svg class="w-4 h-4 inline mr-1 mb-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
           Home
         </a>
-        <a href="#" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
+        <a href="#" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
           <svg class="w-4 h-4 inline mr-1 mb-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"></path></svg>
           Reports
         </a>
-        <a href="" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
+        <a href="" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
           <svg class="w-4 h-4 inline mr-1 mb-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
           Items
         </a>
-        <a href="" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium bg-rose-100 text-red-600 font-medium">
+        <a href="" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium bg-rose-100 text-red-600 font-medium">
           <svg class="w-4 h-4 inline mr-1 mb-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
           Inventory Management
         </a>
-        <a href="" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
+        <a href="" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
           <svg class="w-4 h-4 inline mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"></path></svg>
           Employees
         </a>
-        <a href="" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
+        <a href="" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
           <svg class="w-4 h-4 inline mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
           Customers
         </a>
         <div class="mb-3 pl-1 mt-3">
           <span class="font-medium">SUPPORT</span>
         </div>
-        <a href="" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
+        <a href="" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
           <svg class="w-4 h-4 inline mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
           Settings
         </a>
-        <a href="" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-200 hover:text-red-600 hover:font-medium">
+        <a href="" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-200 hover:text-red-600 hover:font-medium">
           <svg class="w-4 h-4 inline mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
           Developers
         </a>
-        <a href="" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-200 hover:text-red-600 hover:font-medium">
+        <a href="" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-200 hover:text-red-600 hover:font-medium">
           <svg class="w-4 h-4 inline mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
           FAQ
         </a>
@@ -165,13 +170,32 @@
     </div>
   
     <!-- content -->
-    <div class="flex-1 text-2xl">
-      <div class="flex font-sans shadow py-4 pl-8">
+    <div class="flex-1 text-2xl h-screen bg-gray-50">
+      <div class="flex font-sans shadow py-4 pl-8 bg-white">
         <p class="tracking-tight font-medium text-red-600 text-xl">
           Inventory Management
         </p>
         <svg class="w-5 h-5 inline mt-1.5 ml-1.5 fill-current text-rose-300 rounded-full ring-1 ring-pink-100 cursor-pointer" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
       </div>  
+      <div class="w-full p-7 pt-20">
+        <div class="w-64 shadow-sm bg-white font-medium">
+          <nav class="text-sm text-red-400">
+            <a href="#" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
+              Item list
+            </a>
+            <a href="#" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
+              Categories
+            </a>
+            <a href="" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium">
+              Modifiers
+            </a>
+            <a href="" tabindex="-1" class="block py-2 px-4 rounded transition duration-200 hover:bg-rose-100 hover:text-red-600 hover:font-medium bg-rose-100 text-red-600 font-medium">
+              Discounts
+            </a>
+          </nav>
+        </div>
+        {{-- <div class="col-span-5 shadow-sm bg-white">&nbsp;</div> --}}
+      </div>
       {{-- <div class="top-5">
         <!-- mobile menu button -->
         <button class="rounded-md text-rose-300 hover:text-red-400 focus:outline-none focus:ring-2 focus:ring-white" @click="opensidebar = false">
